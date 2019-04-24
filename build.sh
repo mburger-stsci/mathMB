a63382ada4ee96cda46356b8e04ff62607624ce8
Matthew Burger <mburger@stsci.edu>
2019-04-24 15:43:48 -0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+# Remove old versions
+rm dist/*.*
+
+# Build the distribution
+python setup.py sdist bdist_wheel bdist_egg
+
+# Push to github
+echo "Enter commit comment: "
+read comment
+git add --all
+git commit -m "$comment"
+git push
+
+twine upload dist/*
---
+++ mathMB/__init__.py
@@ -0,0 +1,11 @@
+from .interpu import interpu
+from .minmaxmean import minmaxmean
+from .randomdeviates import (random_deviates_1d, random_deviates_2d,
+                             random_gaussian)
+from .rotation_matrix import rotation_matrix
+
+
+name = 'mymath'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '0.1.0'
