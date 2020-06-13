3bffe8d57be2049d4a92507f7e21030d457b5276
Matthew Burger <mburger@stsci.edu>
2020-06-13 15:47:25 -0400

    Added fit_model

--- build/lib/mathMB/__init__.py
+++ build/lib/mathMB/__init__.py
@@ -2,9 +2,10 @@ from .interpu import interpu
 from .minmaxmean import minmaxmean
 from .randomdeviates import random_deviates_1d, random_deviates_2d
 from .rotation_matrix import rotation_matrix
+from .fit_model import fit_model
 
 
 name = 'mathMB'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.0.9'
+__version__ = '1.2.0'
