4f75bf220c368bdf706eebc352bd12fc78c91cbd
Matthew Burger <mburger@stsci.edu>
2020-08-14 09:47:21 -0400

    Added smooth function

--- build/lib/mathMB/__init__.py
+++ build/lib/mathMB/__init__.py
@@ -2,10 +2,11 @@ from .interpu import interpu
 from .minmaxmean import minmaxmean
 from .randomdeviates import random_deviates_1d, random_deviates_2d
 from .rotation_matrix import rotation_matrix
+from .smooth import smooth
 from .fit_model import fit_model
 
 
 name = 'mathMB'
 __author__ = 'Matthew Burger'
 __email__ = 'mburger@stsci.edu'
-__version__ = '1.2.0'
+__version__ = '1.3'
